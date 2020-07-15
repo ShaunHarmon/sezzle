775cf73e49a300c6983c021f4d1fad99b1cab76c
ShaunHarmon <shaun.harmon@gmail.com>
2020-07-15 14:59:55 -0500

    New commit
    create project, add firebase

---
+++ calc-app/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD-X5kAel_j5vR9PdxIVvgFrETFkQcVQ0o',
+    authDomain: 'sezzle-calc-app.firebaseapp.com',
+    databaseURL: 'https://sezzle-calc-app.firebaseio.com',
+    projectId: 'sezzle-calc-app',
+    storageBucket: 'sezzle-calc-app.appspot.com',
+    messagingSenderId: '570163824151',
+    appId: '1:570163824151:web:81b9e06f37a5cf74817e52',
+    measurementId: 'G-37VMXSPKW3'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
